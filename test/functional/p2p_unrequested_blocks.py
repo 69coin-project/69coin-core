d1b3f0291f9f65ce30de3c0d101b3199cb0c1baa
buntheun keun <kbuntheun@yahoo.com>
2018-06-04 12:18:49 +0700

    Add files via upload

--- test/functional/p2p_unrequested_blocks.py
+++ test/functional/p2p_unrequested_blocks.py
@@ -60,8 +60,8 @@ from test_framework.blocktools import create_block, create_coinbase, create_tran
 class AcceptBlockTest(BitcoinTestFramework):
     def add_options(self, parser):
         parser.add_option("--testbinary", dest="testbinary",
-                          default=os.getenv("LITECOIND", "litecoind"),
-                          help="litecoind binary to test")
+                          default=os.getenv("KH69COIND", "kh69coind"),
+                          help="kh69coind binary to test")
 
     def set_test_params(self):
         self.setup_clean_chain = True
